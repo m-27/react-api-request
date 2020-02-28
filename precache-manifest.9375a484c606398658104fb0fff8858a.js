521cceea4b05cb8cadf9e00ab24794fbc9df8f4d
melanie <mel.92527@gmail.com>
2020-02-28 13:26:50 +0800

    Updates

---
+++ precache-manifest.9375a484c606398658104fb0fff8858a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8addd96cb314f68048fc6db1f096074",
+    "url": "/react-api-request/index.html"
+  },
+  {
+    "revision": "0ec47884892d6c0adbaa",
+    "url": "/react-api-request/static/css/main.38c92850.chunk.css"
+  },
+  {
+    "revision": "bb7e1c110b5792123ad7",
+    "url": "/react-api-request/static/js/2.03cfc43c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-api-request/static/js/2.03cfc43c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ec47884892d6c0adbaa",
+    "url": "/react-api-request/static/js/main.af792cc6.chunk.js"
+  },
+  {
+    "revision": "31c7c9a0ab3a98d4d858",
+    "url": "/react-api-request/static/js/runtime-main.7b4ff151.js"
+  }
+]);
