4eb08d4d09a7528d26266535708c4b2dbc33c13c
melanie <mel.92527@gmail.com>
2020-02-29 23:21:51 +0800

    Updates

---
+++ precache-manifest.ebe916b71fc01813c0ec02db9aefc2a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05bd14398f0a00cb3b5b0c216302da0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48c7158c2414c4e73d1f",
+    "url": "/static/css/main.38c92850.chunk.css"
+  },
+  {
+    "revision": "bb7e1c110b5792123ad7",
+    "url": "/static/js/2.03cfc43c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.03cfc43c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c7158c2414c4e73d1f",
+    "url": "/static/js/main.0a793d1d.chunk.js"
+  },
+  {
+    "revision": "755a0a3965ec926ac5a0",
+    "url": "/static/js/runtime-main.4a497a7f.js"
+  }
+]);
